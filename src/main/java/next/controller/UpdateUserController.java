a4840ff9ad7b18fb9b3af8ecbea92ec6e66bf67a
김경석 <0xe82de@gmail.com>
2023-01-14 18:09:08 +0900

    update: 7장 실습

--- src/main/java/next/controller/UpdateUserController.java
+++ src/main/java/next/controller/UpdateUserController.java
@@ -3,6 +3,7 @@ package next.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import next.dao.UserDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +11,20 @@ import core.db.DataBase;
 import core.mvc.Controller;
 import next.model.User;
 
+import java.sql.SQLException;
+
 public class UpdateUserController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(UpdateUserController.class);
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        User user = DataBase.findUserById(req.getParameter("userId"));
+        UserDao userDao = new UserDao();
+        User user = null;
+        try {
+            user = userDao.findByUserId(req.getParameter("userId"));
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+        }
         if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
             throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
         }
